692282f63ac74b9f1bcf5461e17bfc9f61a8b84b
Dark05knight <156595841+Dark05knight@users.noreply.github.com>
2024-11-28 21:10:20 +0300

    Add files via upload

---
+++ plotter (2).py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+
+    #Описание:   
+    #Функция сохраняет построенный график в файл.
+    #Аргументы:
+    #- data (list): Данные для построения графика.    
+    #- fname (str): Имя файла для сохранения графика.
+    #Примечание:
+    #Формат файла будет определен на основе расширения имени файла. 
+def plot_sorted_data(sorted_data: list):
+    try:
+         plt.plot(sorted_data)
+         plt.title('Sorted data')
+         plt.xlabel('Index')
+         plt.ylabel('Meaning')
+         plt.show()
+         print(f"TEST.")
+    except Exception as e:
+        print(f"Error while plotting : {e}")
